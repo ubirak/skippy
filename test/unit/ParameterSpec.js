be725bcaf3ce12c32561c15b9f1a33ee280d66c5
Armand Abric <armand@forgebinaire.net>
2015-06-07 15:08:39 +0200

    Add tests.

---
+++ test/unit/ParameterSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Parameter = require('./../../src/Parameter');
+
+
+describe('Parameter', function() {
+    it('should have name and a value', function() {
+        var parameter = new Parameter('foo', 'bar');
+
+        expect(parameter.getName()).to.equal('foo');
+        expect(parameter.getValue()).to.equal('bar');
+    });
+});
