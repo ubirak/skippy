3f27c2c3876612a066a2a85311737f90d161a438
Armand Abric <armand@verylastroom.com>
2016-05-10 09:09:10 +0200

    Always expose the container `mockService` method

--- tests/units/ContainerSpec.js
+++ tests/units/ContainerSpec.js
@@ -13,16 +13,6 @@ describe('Container', function () {
         process.env.NODE_ENV = previousProcessEnv;
     });
 
-    /**
-     * @see http://stackoverflow.com/a/16060619
-     * @param {[type]} module
-     * @return {[type]}
-     */
-    function requireUncached(module) {
-        delete require.cache[require.resolve(module)]
-        return require(module)
-    }
-
     it('should throw a error when trying to mock an undefined service', function () {
         var serviceDefinitionCollection = sinon.createStubInstance(ServiceDefinitionCollection);
         var parameterCollection = sinon.createStubInstance(ParameterCollection);
@@ -32,28 +22,6 @@ describe('Container', function () {
         expect(function () {
             container.mockService('foo', function () {});
         }).to.throw('Unknown service "foo".');
-    });
-
-    it('should provide an api to mock a service in test environement', function () {
-        var serviceDefinitionCollection = sinon.createStubInstance(ServiceDefinitionCollection);
-        var parameterCollection = sinon.createStubInstance(ParameterCollection);
-
-        process.env.NODE_ENV = 'test';
-        var FreshContainer = requireUncached('./../../src/Container');
-
-        var container = new FreshContainer(serviceDefinitionCollection, parameterCollection);
-        expect(container).to.have.property('mockService');
-    });
-
-    it('should not provide an api to mock a service in production environement', function () {
-        var serviceDefinitionCollection = sinon.createStubInstance(ServiceDefinitionCollection);
-        var parameterCollection = sinon.createStubInstance(ParameterCollection);
-
-        process.env.NODE_ENV = 'production';
-        var FreshContainer = requireUncached('./../../src/Container');
-
-        var container = new FreshContainer(serviceDefinitionCollection, parameterCollection);
-        expect(container).to.not.have.property('mockService');
     });
 
     it('should check the service cyclic dependencies if the container validation activated', function () {
